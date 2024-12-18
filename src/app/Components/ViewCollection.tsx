4f93008e67347ce7a61993b34ac978cce31a0263
Ammar Ahmed Khan <161769688+ammarakk@users.noreply.github.com>
2024-12-18 20:02:53 +0500

    Add files via upload

---
+++ src/app/Components/ViewCollection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllProducts from './AllProducts'
+
+const ViewCollection = () => {
+  return (
+    <div>
+      <AllProducts/>
+    </div>
+  )
+}
+
+export default ViewCollection
